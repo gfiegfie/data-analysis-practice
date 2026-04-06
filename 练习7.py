bc33950d6e6580587ed2cd539a593a2261737ce7
gfiegfie <gfiefie@163.com>
2026-04-06 23:52:14 +0800

    练习7

---
+++ 练习7.py
@@ -0,0 +1,74 @@
+print(7 + 3)
+print(7 - 3)
+print(7 * 3)
+print(7 / 3)
+print(7 // 3)
+print(7 % 3)
+print(7**3)
+print(7 * (3 + 2) * 4)
+print(7 * ((3 + 5) % 2))
+print(7 * +3)
+print(7 * -3)
+print(7.0 + 3)
+print(7 + 3.0)
+print(7.0 + 3.0)
+print(0b100)
+print(0o100)
+print(0x100)
+print(100)
+print(38_239_521_489_247)
+x = 17
+y = 52
+print(x + y)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(int(x))
+print(float(x))
+print(int(y))
+print(float(y))
+print(str(x))
+print(str(y))
+print(bool(x))
+print(bool(y))
+print(bool(0))
+print(bool(0.0))
+print(bool(""))
+print(bool(None))
+# Output:
+# 10
+# 4
+# 21
+# 2.3333333333333335
+# 2
+# 1
+# 343
+# 144
+# 17
+# 52
+# 17.0
+# 17.0
+# 100
+# 38239521489247
+# 79
+# <class 'int'>
+# 52
+# <class 'int'>
+# 17
+# <class 'int'>
+# 52
+# <class 'int'>
+# 17.0
+# 52.0
+# 17.0
+# 52.0
+# 17
+# 52
+# 17
+# 52
+# 0
+# False
+# False
+# False
+# False
